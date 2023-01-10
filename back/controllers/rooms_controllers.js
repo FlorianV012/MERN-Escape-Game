e8ddfe496122abb1ee3e68d33f163ae4c476ec5d
FlorianV <fvilmot@gmail.com>
2023-01-10 10:27:43 +0100

    add controller for read one room

--- back/controllers/rooms_controllers.js
+++ back/controllers/rooms_controllers.js
@@ -10,3 +10,14 @@ exports.indexRooms = (req, res, next) => {
       res.status(400).json({ error });
     });
 };
+
+// Show one room
+exports.getOneRoom = (req, res, next) => {
+  Room.findOne({ _id: req.params.id })
+    .then((room) => {
+      res.status(200).json(room);
+    })
+    .catch((error) => {
+      res.status(404).json({ error });
+    });
+};
