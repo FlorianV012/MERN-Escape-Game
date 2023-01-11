f31b926833d13e2945a5d9e565114758167b68e7
fvilmot <fvilmot@gmail.com>
2023-01-11 17:01:23 +0100

    update users controllers with brcrypt & jwt

--- back/controllers/users_controllers.js
+++ back/controllers/users_controllers.js
@@ -1,25 +1,52 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const User = require("../models/users_models");
 
 exports.signup = (req, res, next) => {
-  const user = new User({
-    ...req.body,
-  });
-  user
-    .save()
-    .then((user) => {
-      res.status(200).json(user);
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        birthDate: req.body.birthDate,
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => {
-      res.status(400).json({ error });
+      res.status(500).json({ error });
     });
 };
 
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
-      res.status(200).json(user);
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, process.env.SECRET_TOKEN, {
+              expiresIn: "24h",
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
     })
     .catch((error) => {
-      res.status(404).json({ error });
+      res.status(500).json({ error });
     });
 };
